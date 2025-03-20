2256a26569557403840b3abb9d8deb09a5c8f309
imran khawaja <imrank1@gmail.com>
2025-03-20 00:10:08 -0700

    more cors config

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,2 +1,20 @@
 // This file is generated during build, but we're creating it manually for deployment
-module.exports = require('../dist/server'); 
+const app = require('../dist/server');
+
+// Handle OPTIONS preflight requests
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+  res.sendStatus(204);
+});
+
+// Add CORS headers to all responses
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+  next();
+});
+
+module.exports = app; 
