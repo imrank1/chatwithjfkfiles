b1bd223933bcdddb265ee5976445192f2fa3f28b
imran khawaja <imrank1@gmail.com>
2025-03-20 11:40:43 -0700

    try again

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,1 @@
-// This file is generated during build, but we're creating it manually for deployment
-const app = require('../dist/server');
-
-// Export the app for serverless deployment
-module.exports = app; 
+module.exports = require('../dist/server');
